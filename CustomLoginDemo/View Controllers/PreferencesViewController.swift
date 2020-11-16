85fc7816d38cf2670a7929370bf16f902efa4f1b
rohitt-ravi <rohitravi2001@berkeley.edu>
2020-11-15 20:55:37 -0800

    added preferences array

---
+++ CustomLoginDemo/View Controllers/PreferencesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PreferencesViewController.swift
+//  CustomLoginDemo
+//
+//  Created by Rohit Ravi on 11/15/20.
+//  Copyright © 2020 Rohit Ravi. All rights reserved.
+//
+
+import UIKit
+
+class PreferencesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
