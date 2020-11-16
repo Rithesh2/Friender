0bc64a425a1eb24c5a3c94458d08f813550c178e
rohitt-ravi <rohitravi2001@berkeley.edu>
2020-11-15 23:08:01 -0800

    with marias code

--- CustomLoginDemo/View Controllers/NewMatchesViewController.swift
+++ CustomLoginDemo/View Controllers/NewMatchesViewController.swift
@@ -1,31 +1,54 @@
-//
-//  NewMatchesViewController.swift
-//  CustomLoginDemo
-//
-//  Created by Divya Kunisetty on 11/12/20.
-//  Copyright © 2020 Rohit Ravi. All rights reserved.
-//
+ import UIKit
 
-import UIKit
-
-class NewMatchesViewController: UIViewController {
+ class NewMatchesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    //how do we pass our user names? (question)
+    let userNames = MyUser(uid: "")
+    
+    //how do we pass our user pictures? (question)
+    let userPictures = []
+    
+    
+    //how do we pass our user contacts? (question)
+    let contacts = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tbvc = self.tabBarController  as! CustomTabBarController
-        let fUser = tbvc.fUser
-        print("newMatches" + fUser!.uid)
-        // Do any additional setup after loading the view.
+        
+        //register our custom cell for a tableview
+        self.tableView.register(UINib(nibName: "CustomUITableViewCell", bundle: nil), forCellReuseIdentifier: "customUser")
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(  tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //we want number of users update automatically
+        return userNames.count
     }
-    */
 
+    func tableView(  tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =
+            tableView.dequeueReusableCell(withIdentifier: "customUser", for: indexPath) as! CustomTableViewCell
+        //can we say "cell.userPicture.image"? (question)
+        cell.userPicture.image = userPictures[indexPath.row]
+        cell.userName.text = userName[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let contact = contacts[indexPath.row]
+        print(contact)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90.0
+    }
+ }
+    
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userPicture: UIImageView!
 }
