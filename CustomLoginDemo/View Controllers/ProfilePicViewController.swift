b8ca0b67cda59abbc5b01ea5fb5c3a2e0e476b55
Divya Kunisetty <divyakunisett@berkeley.edu>
2020-11-22 21:45:31 -0800

    updated preferences

--- CustomLoginDemo/View Controllers/ProfilePicViewController.swift
+++ CustomLoginDemo/View Controllers/ProfilePicViewController.swift
@@ -10,3 +10,24 @@ import UIKit
 import FirebaseAuth
 import FirebaseFirestore
 import Firebase
+
+class ProfilePicViewController: UIViewController {
+
+    @IBOutlet weak var uploadProfilePicLabel: UILabel!
+    
+    var str = "please upload your profile picture"
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        let gesture = UITapGestureRecognizer(target:self, action: #selector(onClickView))
+        self.view.addGestureRecognizer(gesture)
+    }
+    
+    @objc
+    func onClickView() {
+        for i in str{
+            uploadProfilePicLabel.text! += "\(i)"
+            RunLoop.current.run(until: Date()+0.12)
+        }
+    }
+
+}
