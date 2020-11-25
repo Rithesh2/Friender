fccf6c78ada3ce0f649c9e76981c5e4ec6ab443b
Rithesh Kannan <ritheshkannan@Ritheshs-MacBook-Pro.local>
2020-11-24 21:13:43 -0800

    x

--- Pods/Target Support Files/Pods-CustomLoginDemo-CustomLoginDemoUITests/Pods-CustomLoginDemo-CustomLoginDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CustomLoginDemo_CustomLoginDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CustomLoginDemo_CustomLoginDemoUITests
-@end
