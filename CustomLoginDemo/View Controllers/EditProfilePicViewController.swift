44544e973ca914e7b206d46413ab3f68d80ce2e3
Rithesh Kannan <ritheshkannan@Ritheshs-MacBook-Pro.local>
2020-11-27 13:31:40 -0800

    pic

--- CustomLoginDemo/View Controllers/EditProfilePicViewController.swift
+++ CustomLoginDemo/View Controllers/EditProfilePicViewController.swift
@@ -2,7 +2,7 @@
 //  EditProfilePicViewController.swift
 //  CustomLoginDemo
 //
-//  Created by Divya Kunisetty on 11/22/20.
+//  Created by Rithesh Kannan on 11/27/20.
 //  Copyright © 2020 Rohit Ravi. All rights reserved.
 //
 
@@ -11,8 +11,43 @@ import FirebaseAuth
 import FirebaseFirestore
 import Firebase
 
+class EditProfilePicViewController: UIViewController {
+
+    @IBOutlet weak var pictureLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+  
+        
+        
+        func picture(){
+            pictureLabel.isUserInteractionEnabled = true
+            
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(presentPicker))
+        pictureLabel.addGestureRecognizer(tapGesture)
+    }
+
+
+    @objc func presentPicker(){
+        let picker = UIImagePickerController()
+        
+        picker.sourceType  = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+    }
 
 
 
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
+}
--- CustomLoginDemo/View Controllers/ProfileViewController.swift
+++ CustomLoginDemo/View Controllers/ProfileViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class ProfileViewController: UIViewController {
   
+    @IBOutlet weak var profilePicture: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     var fUser: MyUser? = nil
     override func viewDidLoad() {
         super.viewDidLoad()
+        profilePicture.layer.cornerRadius = 60
+        profilePicture.clipsToBounds = true
         let tbvc = self.tabBarController  as! CustomTabBarController
         fUser = tbvc.fUser
         let userFullName = fUser!.firstName + " " + fUser!.lastName
@@ -23,7 +26,15 @@ class ProfileViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func editProfilePicture(_ sender: Any) {
+        let picker = UIImagePickerController()
+        
+        picker.sourceType  = .photoLibrary
+        picker.allowsEditing = true
+        picker.delegate = self
+        self.present(picker, animated: true, completion: nil)
+    }
+    
     @IBAction func editPreferenes(_ sender: Any) {
         performSegue(withIdentifier: "editPreferencesSegue", sender: self)
     }
@@ -48,3 +59,17 @@ class ProfileViewController: UIViewController {
 
 }
 
+extension ProfileViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imageSelected = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+            profilePicture.image = imageSelected
+        }
+        
+        if let imageOriginal = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            profilePicture.image = imageOriginal
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
