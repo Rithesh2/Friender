6d296674ee452e9f43550723720c9941b48f6781
rohitt-ravi <rohitravi2001@berkeley.edu>
2020-12-05 11:52:25 -0800

    elements

--- CustomLoginDemo/View Controllers/SelectElementViewController.swift
+++ CustomLoginDemo/View Controllers/SelectElementViewController.swift
@@ -14,6 +14,16 @@ import Firebase
 class SelectElementViewController: UIViewControllerX {
     var fUser: MyUser? = nil
     
+    var earth_selected = false
+    var fire_selected = false
+    var water_selected = false
+    var air_selected = false
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var earthB: UIButton!
+    @IBOutlet weak var waterB: UIButton!
+    @IBOutlet weak var airB: UIButton!
+    @IBOutlet weak var fireB: UIButton!
     @IBOutlet weak var selectAnimalLabel: UILabel!
     
     @IBOutlet weak var enterAnswer: UITextField!    
@@ -39,9 +49,15 @@ class SelectElementViewController: UIViewControllerX {
     @objc private func moveToNext() {
 
         // our custom stuff
-        self.fUser!.Element = self.enterAnswer.text ?? String()
+        if(fUser!.Element == "")
+        {
+            errorLabel.text = "Please make sure to choose one Element"
+            errorLabel.isHidden = false
+        }
+        else
+        {
         let db = Firestore.firestore()
-        db.collection("users").document(self.fUser!.uid).updateData(["Element": self.enterAnswer.text ?? String()]){ (error) in
+        db.collection("users").document(self.fUser!.uid).updateData(["Element": self.fUser!.Element]){ (error) in
                                                     if error != nil{
                                                         print("error")
                                                     }
@@ -50,9 +66,86 @@ class SelectElementViewController: UIViewControllerX {
         let vc = storyboard?.instantiateViewController(withIdentifier: "selectPrefContact") as! SelectPrefContactViewController
         vc.fUser = self.fUser
         navigationController?.pushViewController(vc, animated: true)
+        }
        }
   
-       }
+    @IBAction func fireButton(_ sender: Any) {
+        guard let button = sender as? UIButton else { return }
+        if !fire_selected
+        {
+            fUser!.Element = "fire"
+            button.tintColor = .systemTeal
+            earthB.tintColor = .lightGray
+            waterB.tintColor = .lightGray
+            airB.tintColor = .lightGray
+                fire_selected = true
+            
+        }
+        else{
+            fUser!.Element = ""
+            button.tintColor = .lightGray
+            fire_selected = false
+
+        }
+    }
+    @IBAction func airButton(_ sender: Any) {
+        guard let button = sender as? UIButton else { return }
+        if !air_selected
+        {
+            fUser!.Element = "air"
+            button.tintColor = .systemTeal
+            earthB.tintColor = .lightGray
+            waterB.tintColor = .lightGray
+            fireB.tintColor = .lightGray
+                air_selected = true
+            
+        }
+        else{
+            fUser!.Element = ""
+            button.tintColor = .lightGray
+            air_selected = false
+
+        }
+    }
+    @IBAction func waterButton(_ sender: Any) {
+        guard let button = sender as? UIButton else { return }
+        if !water_selected
+        {
+            fUser!.Element = "water"
+            button.tintColor = .systemTeal
+            earthB.tintColor = .lightGray
+            fireB.tintColor = .lightGray
+            airB.tintColor = .lightGray
+                water_selected = true
+            
+        }
+        else{
+            fUser!.Element = ""
+            button.tintColor = .lightGray
+            water_selected = false
+
+        }
+    }
+    @IBAction func earthButton(_ sender: Any) {
+        guard let button = sender as? UIButton else { return }
+        if !earth_selected
+        {
+            fUser!.Element = "earth"
+            button.tintColor = .systemTeal
+            waterB.tintColor = .lightGray
+            fireB.tintColor = .lightGray
+            airB.tintColor = .lightGray
+                water_selected = true
+            
+        }
+        else{
+            fUser!.Element = ""
+            button.tintColor = .lightGray
+            earth_selected = false
+
+        }
+    }
+}
 
     /*
     // MARK: - Navigation
